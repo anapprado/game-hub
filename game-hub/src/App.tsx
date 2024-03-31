6575c6bbb0148bf699adc6ed2bcdfb8a10e03eb7
Ana Prado <153234400+anapprado@users.noreply.github.com>
2024-03-30 20:35:57 -0500

    build a navbar

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,8 +1,33 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
-return <Button colorScheme='blue'>Button</Button>;
+return <Grid
+templateAreas={`"header header"
+                "nav main"
+                "nav footer"`}
+gridTemplateRows={'50px 1fr 30px'}
+gridTemplateColumns={'150px 1fr'}
+h='200px'
+gap='1'
+color='blackAlpha.700'
+fontWeight='bold'
+>
+<GridItem pl='2' bg='orange.200' area={'header'}>
+  <NavBar />
+</GridItem>
+<GridItem pl='2' bg='pink.300' area={'nav'}>
+  
+</GridItem>
+<GridItem pl='2' bg='green.300' area={'main'}>
+  Main
+</GridItem>
+<GridItem pl='2' bg='blue.300' area={'footer'}>
+  Footer
+</GridItem>
+</Grid>
+
 }
 
 export default App
